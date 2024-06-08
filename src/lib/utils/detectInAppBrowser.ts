e09bc420740b8147ef52cbdb263d68d376588fda
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2024-06-08 11:20:58 +0530

    feat: better detection for webview

--- src/lib/utils/detectInAppBrowser.ts
+++ src/lib/utils/detectInAppBrowser.ts
@@ -19,24 +19,24 @@ export function isInAppBrowser() {
 export function detectInAppBrowser() {
 	const navigator = window.navigator;
 	const userAgent = navigator.userAgent;
-	const normalizedUserAgent = userAgent.toLowerCase();
+	// const normalizedUserAgent = userAgent.toLowerCase();
 	// const standalone = navigator?.standalone;
 
-	const isIos =
-		/ip(ad|hone|od)/.test(normalizedUserAgent) ||
-		(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
-	const isAndroid = /android/.test(normalizedUserAgent);
-	const isSafari = /safari/.test(normalizedUserAgent);
-	const isWebview = (isAndroid && /; wv\)/.test(normalizedUserAgent)) || (isIos && !isSafari);
+	// const isIos =
+	// 	/ip(ad|hone|od)/.test(normalizedUserAgent) ||
+	// 	(navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+	// const isAndroid = /android/.test(normalizedUserAgent);
+	// const isSafari = /safari/.test(normalizedUserAgent);
+	const isWebview = /webview|wv|ip((?!.*Safari)|(?=.*like Safari))/i.test(userAgent);
 
-	let mobileView = false;
-	// if (isIos || isAndroid) {
+	// let mobileView = false;
+	// // if (isIos || isAndroid) {
+	// // 	mobileView = true;
+	// // }
+	// if (isWebview) {
 	// 	mobileView = true;
 	// }
-	if (isWebview) {
-		mobileView = true;
-	}
-	return { userAgent: userAgent, inAppBrowser: mobileView };
+	return { userAgent: userAgent, inAppBrowser: isWebview };
 	// const osText = isIos ? 'iOS' : isAndroid ? 'Android' : 'Other';
 	// const webviewText = isWebview ? 'Yes' : 'No';
 }
