b64cd80cbf5212e6ab6f72f8bbdd5367f111f68a
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2024-06-08 12:36:12 +0530

    feat: retry

--- src/lib/components/NewHeader.svelte
+++ src/lib/components/NewHeader.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { detectInAppBrowser } from '../utils/detectInAppBrowser';
 	import OpenDefaultBrowserModal from './OpenDefaultBrowserModal.svelte';
 
