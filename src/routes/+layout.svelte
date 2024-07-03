fa0f2d366955e30799b5d56bbe5fbbbe240846db
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2024-07-03 23:30:14 +0530

    feat: removed references to og url

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,17 +42,17 @@
 
 <svelte:head>
 	{#if !dev}
-		<script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
+		<!-- <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script> -->
 	{/if}
 
 	<!-- <script
 		src={`https://www.google.com/recaptcha/enterprise.js?render=${PUBLIC_RECAPTCHA_KEY}`}
 	></script> -->
 	<title>Career Chakra - Your AI-Powered Job Matching Service</title>
-	{#if !dev}
-		<base href="https://www.careerchakra.com/" />
-	{/if}
-	<meta property="og:url" content="https://www.careerchakra.com" />
+	<!-- {#if !dev} -->
+	<!-- <base href="https://www.careerchakra.com/" /> -->
+	<!-- {/if} -->
+	<!-- <meta property="og:url" content="https://www.careerchakra.com" /> -->
 	<meta property="og:image" content="/chakraImg.png" />
 
 	<meta
@@ -70,7 +70,7 @@
 		content="Discover your next job with Career Chakra. Our AI reads your resume and finds the perfect job opportunities for you."
 	/>
 
-	<meta property="og:url" content="https://www.careerchakra.com" />
+	<!-- <meta property="og:url" content="https://www.careerchakra.com" /> -->
 	<meta name="twitter:card" content="/chakraImg.png" />
 	<meta name="twitter:title" content="Career Chakra - Your AI-Powered Job Matching Service" />
 	<meta
