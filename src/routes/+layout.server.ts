ad2f86a881deaf69d11582f555350fbff5a3a149
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2024-09-14 23:54:57 +0530

    feat: comment out env values

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,11 +1,10 @@
-import { TURSO_DB, TURSO_TOKEN } from '$env/static/private';
 import { db } from '$lib/server/drizzle/turso-db';
 import { userResumeTable } from '$lib/server/drizzle/turso-schema';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
 	try {
-		const res = await db.select().from(userResumeTable).limit(4);
+		const res = await db.select().from(userResumeTable).limit(1);
 		return {
 			user: locals.user,
 			pathname: url.pathname,
@@ -13,7 +12,7 @@ export const load: LayoutServerLoad = async ({ locals, url }) => {
 		};
 	} catch (error) {
 		return {
-			url: { TURSO_DB, TURSO_TOKEN },
+			// url: { TURSO_DB, TURSO_TOKEN },
 			user: locals.user,
 			pathname: url.pathname,
 			error: JSON.stringify(error)
