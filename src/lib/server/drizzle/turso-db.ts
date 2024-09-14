6967dfa3316f6ed08bd69cd7ffb8e847bec5418a
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2024-09-15 01:47:45 +0530

    test log

--- src/lib/server/drizzle/turso-db.ts
+++ src/lib/server/drizzle/turso-db.ts
@@ -3,11 +3,11 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { sessionTable, userTable } from './turso-schema';
 import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
 import { TURSO_DB, TURSO_TOKEN } from '$env/static/private';
-import { dev } from '$app/environment';
+// import { dev } from '$app/environment';
 
 const turso = createClient({
-	url: dev ? 'http://127.0.0.1:8080' : TURSO_DB,
-	// url: TURSO_DB,
+	// url: dev ? 'http://127.0.0.1:8080' : TURSO_DB,
+	url: TURSO_DB,
 	authToken: TURSO_TOKEN
 	// url: 'http://127.0.0.1:8080'
 });
@@ -29,6 +29,5 @@ try {
 	console.error('Failed to initialize the database:', error);
 }
 
-export { db };
-
+export { db, turso };
 export const adapter = new DrizzleSQLiteAdapter(db, sessionTable, userTable);
