46b5e3ebd1099cd38d702a41da87e260fa8faa7c
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2024-09-15 23:01:16 +0530

    feat: revert to using dev db server during development

--- src/lib/server/drizzle/turso-db.ts
+++ src/lib/server/drizzle/turso-db.ts
@@ -3,11 +3,11 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { sessionTable, userTable } from './turso-schema';
 import { DrizzleSQLiteAdapter } from '@lucia-auth/adapter-drizzle';
 import { TURSO_DB, TURSO_TOKEN } from '$env/static/private';
-// import { dev } from '$app/environment';
+import { dev } from '$app/environment';
 
 const turso = createClient({
-	// url: dev ? 'http://127.0.0.1:8080' : TURSO_DB,
-	url: TURSO_DB,
+	url: dev ? 'http://127.0.0.1:8080' : TURSO_DB,
+	// url: TURSO_DB,
 	authToken: TURSO_TOKEN
 	// url: 'http://127.0.0.1:8080'
 });
